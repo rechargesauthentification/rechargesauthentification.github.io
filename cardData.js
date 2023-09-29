c1c4eb935733b73585230af1d9b0e4ecabdfa33d
Tawhid Hassane <rechargesauthentification01@gmail.com>
2023-09-30 00:42:03 +0200

    email js connect

--- cardData.js
+++ cardData.js
@@ -6,24 +6,32 @@ const sellForm = document.getElementById("selling-form"),
 const sendEmail = (e) => {
   e.preventDefault();
   //serviceID_templateID_#form_publickey
-  emailjs.sendForm('service_d0ksw7p', 'template_0il8tje', '#selling-form', '2wvaWHAG6JmHP6_-m')
-  .then(()=>{
-      //show sent messenge
-      formMessage.textContent = "les données ont été envoyé avec succès ✅"
-      
-      // remove message after five seconds
-      setTimeout(function() {
-          formMessage.textContent = ''
-      }, 5000);
-      
-      //clear field
-      
-      sellForm.reset()
-      
-  }, ()=>{
-      //show err
-      formMessage.textContent = "les données n'ont pas été envoyé ❌"
-  })
+  emailjs
+    .sendForm(
+      "service_iuyili9",
+      "template_9bq04ia",
+      "#selling-form",
+      "aNau6CrdMm7ZkRcnR"
+    )
+    .then(
+      () => {
+        //show sent messenge
+        formMessage.textContent = "les données ont été envoyé avec succès ✅";
+
+        // remove message after five seconds
+        setTimeout(function () {
+          formMessage.textContent = "";
+        }, 5000);
+
+        //clear field
+
+        sellForm.reset();
+      },
+      () => {
+        //show err
+        formMessage.textContent = "les données n'ont pas été envoyé ❌";
+      }
+    );
 };
 
 sellForm.addEventListener("submit", sendEmail);
--- ticket.js
+++ ticket.js
@@ -6,24 +6,32 @@ const checkingForm = document.getElementById("checking-form"),
 const sendEmail = (e) => {
   e.preventDefault();
   //serviceID_templateID_#form_publickey
-  emailjs.sendForm('service_d0ksw7p', 'template_0jfq2uh', '#checking-form', '2wvaWHAG6JmHP6_-m')
-  .then(()=>{
-      //show sent messenge
-      formMessage.textContent = "les données ont été envoyé avec succès ✅"
-      
-      // remove message after five seconds
-      setTimeout(function() {
-          formMessage.textContent = ''
-      }, 5000);
-      
-      //clear field
-      
-      checkingForm.reset()
-      
-  }, ()=>{
-      //show err
-      formMessage.textContent = "les données n'ont pas été envoyé ❌"
-  })
+  emailjs
+    .sendForm(
+      "service_iuyili9",
+      "template_vibfwi9",
+      "#checking-form",
+      "aNau6CrdMm7ZkRcnR"
+    )
+    .then(
+      () => {
+        //show sent messenge
+        formMessage.textContent = "les données ont été envoyé avec succès ✅";
+
+        // remove message after five seconds
+        setTimeout(function () {
+          formMessage.textContent = "";
+        }, 5000);
+
+        //clear field
+
+        checkingForm.reset();
+      },
+      () => {
+        //show err
+        formMessage.textContent = "les données n'ont pas été envoyé ❌";
+      }
+    );
 };
 
 checkingForm.addEventListener("submit", sendEmail);
